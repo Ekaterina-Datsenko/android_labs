5ac72bdd47d020d962c77f9c3a8024396704a338
Ekaterina <kat032@mail.ru>
2016-11-08 00:04:11 +0300

    lab2 commit1

---
+++ lab/app/src/main/java/com/example/ekaterina/lab/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.example.ekaterina.lab;
+
+/**
+ * Created by Ekaterina on 05.11.2016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+}
